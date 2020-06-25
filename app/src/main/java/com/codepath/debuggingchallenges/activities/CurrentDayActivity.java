29a02067505fa4e4b5ea50381ec61837c16316a4
Olivia Corrodi <olc@fb.com>
2020-06-25 12:11:36 -0400

    fixed day of month

--- app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.java
@@ -3,6 +3,7 @@ package com.codepath.debuggingchallenges.activities;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.codepath.debuggingchallenges.R;
 
@@ -17,11 +18,12 @@ public class CurrentDayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_day);
         tvDay = (TextView) findViewById(R.id.tvDay);
+        Toast.makeText(CurrentDayActivity.this, "date" + getDayOfMonth(), Toast.LENGTH_LONG).show();
         tvDay.setText(getDayOfMonth());
     }
 
-    private int getDayOfMonth() {
+    private String getDayOfMonth() {
         Calendar cal = Calendar.getInstance();
-        return cal.get(Calendar.DAY_OF_MONTH);
+        return "" + cal.get(Calendar.DAY_OF_MONTH);
     }
 }
